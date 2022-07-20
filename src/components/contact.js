e7ce96d8bd11149cd2616199c4569a5dc436319f
Kranthi Pamulapati <pamulapati.kranthi@gmail.com>
2022-07-20 09:21:47 +0530

    get contacts

---
+++ src/components/contact.js
@@ -0,0 +1,20 @@
+/**
+ * Wazzap React Native App
+ * kranthipamulapati.com
+ */
+
+import React from "react";
+import {View, FlatList, StyleSheet} from "react-native";
+
+import Page from "./page/Page";
+
+const Contact = ({img, name}) => {
+    
+    return (
+        <Page>
+            
+        </Page>
+    );
+};
+
+export default Contact;
--- src/screens/Contacts.js
+++ src/screens/Contacts.js
@@ -4,6 +4,7 @@
  */
 
 import React, {useContext} from "react";
+import {View, Text, FlatList, Pressable, StyleSheet} from "react-native";
 
 import Page from "../components/page/Page";
 
@@ -14,15 +15,32 @@ import {UserContext} from "../context/userContext";
 const Contacts = ({navigation}) => {
 
     const {authInfo} = useContext(UserContext);
-
     const contacts = useContacts(authInfo.uid);
-    console.log(contacts);
+
+    function renderItem({item}) {
+        return <Pressable style={styles.item}>
+            <Text style={styles.title}>{item.displayName}</Text>
+        </Pressable>
+    }
     
     return (
         <Page>
-            
+            <FlatList data={contacts} renderItem={renderItem} keyExtractor={item => item.rawContactId} />
         </Page>
     );
 };
+
+const styles = StyleSheet.create({
+    item : {
+        borderColor: "gray",
+        borderWidth : 1,
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title : {
+        fontSize: 32,
+    }
+});
 
 export default Contacts;
